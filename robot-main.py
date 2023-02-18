3e9d63fc17685ddd3a6dd4c06446cfc00cde9d0e
drewgillievfx <drewgillievfx@gmail.com>
2023-02-18 15:52:30 -0500

    Creating robot-main.py outline

---
+++ robot-main.py
@@ -0,0 +1,41 @@
+"""
+    Start Date: 20230218
+    Version: 1.0
+    Started by: Drew Gillie
+
+
+"""
+
+# imports
+""" Import all necessary libraries and other scripts into this main file. """
+
+##############################################################################
+##############################################################################
+
+# main
+if __name__ == '__main__':    
+    print(F'\nStarting Robot\n')
+
+    """ 
+    Run Robot Code Here.
+
+    This will look something like
+
+        if match at competition:
+            wait for field signal
+                run autonomous
+            wait for field signal
+                enable teleop
+            wait for field signal
+                disable robot
+        elif practice at competition:
+            switch
+                test auto
+                test teleop
+        else:
+            switch
+                test auto
+                test teleop
+    """
+
+    print(F'\nRobot Ready for Shutdown\n')
