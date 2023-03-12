0fc5897366144b67932d025c3d8054cb7c930973
roadsteadsteve <70551331+roadsteadsteve@users.noreply.github.com>
2023-03-12 14:06:41 -0400

    Fixed things

--- robot.py
+++ robot.py
@@ -13,23 +13,68 @@ class MyRobot(wpilib.TimedRobot):
 
     def robotInit(self):
         # Setup Joystick
-        self.Joystick = wpilib.Joystick(0)
-        
+        self.DriverJoystick = wpilib.Joystick(0)
+        self.OperatorJoystick = wpilib.Joystick(1)
 
         self.LeftFrontMotor = ctre.WPI_TalonSRX(1)
         self.LeftRearMotor = ctre.WPI_TalonSRX(2)
-        self.LeftRearMotor.follow(self.LeftFrontMotor)
 
         self.RightFrontMotor = ctre.WPI_TalonSRX(3)
         self.RightRearMotor = ctre.WPI_TalonSRX(4)
-        self.RightRearMotor.follow(self.RightFrontMotor)
 
         self.JackShaftMotor = rev.CANSparkMax(6,rev.CANSparkMax.MotorType.kBrushless)
-        self.encoder = self.JackShaftMotor.getEncoder()
-        # self.controller = wpilib.PIDController(1, 0, 0, self.encoder, self.JackShaftMotor)
+        self.JackShaftMotor.setInverted(True)
+        self.JackShaftMotor.setIdleMode(rev.CANSparkMax.IdleMode.kBrake)
+
+        self.ArmEncoder = self.JackShaftMotor.getEncoder()
+        self.ArmEncoder.setPosition(0)
 
         self.IntakeMotor = rev.CANSparkMax(7,rev.CANSparkMax.MotorType.kBrushless)
-        self.IntakeMotor.getOutputCurrent()
+
+        self.wantedArmPosition = 0
+        self.armRunningToPosition = False
+
+        # Constants
+        self.MaxDriveSpeed=0.75
+        self.MaxArmSpeed=0.5
+        self.UpperArm = 60
+        self.LowerArm = 40
+        self.armHome = 10
+
+        #Auto inits
+        self.AutoTimer = wpilib.Timer()
+
+    def setDrives(self, leftSpeed: float, rightSpeed: float):
+        self.LeftFrontMotor.set(-leftSpeed)
+        self.LeftRearMotor.set(-leftSpeed)
+        self.RightFrontMotor.set(rightSpeed)
+        self.RightRearMotor.set(rightSpeed)
+
+    # returns if arms are moving for position
+    def setArmPosition(self, position: int):
+        offset = 1
+        currentPosition = self.ArmEncoder.getPosition()
+        print("Current position:", currentPosition, " Wanted: ", position)
+        if(currentPosition > position + offset):
+            self.JackShaftMotor.set(-0.4)
+        elif(currentPosition < position - offset):
+            self.JackShaftMotor.set(0.4)
+        else:
+            self.JackShaftMotor.set(0)
+            self.armRunningToPosition = False
+
+    def autonomousInit(self):
+        self.AutoTimer.reset()
+        self.AutoTimer.start()
+
+    def autonomousPeriodic(self):
+        if(self.AutoTimer.get() < 3):
+            self.setDrives(0.3, 0.3)
+        elif(self.AutoTimer.get() < 10):
+            self.setDrives(-0.3, -0.3)
+        else:
+            self.setDrives(0, 0)
+        
 
     def disabledPeriodic(self):
         self.LeftFrontMotor.disable()
@@ -41,54 +86,48 @@ class MyRobot(wpilib.TimedRobot):
 
     def teleopPeriodic(self):
 
-        self.LEFT_THUMB_LEFTRIGHT = self.Joystick.getRawAxis(0)
-        self.LEFT_THUMB_UPDOWN = self.Joystick.getRawAxis(1)
-        self.RIGHT_THUMB_LEFTRIGHT = self.Joystick.getRawAxis(4)
-        self.RIGHT_THUMB_UPDOWN = self.Joystick.getRawAxis(5)
+        self.LEFT_THUMB_LEFTRIGHT = self.DriverJoystick.getRawAxis(0)
+        self.LEFT_THUMB_UPDOWN = self.DriverJoystick.getRawAxis(1)
+        self.RIGHT_THUMB_LEFTRIGHT = self.DriverJoystick.getRawAxis(4)
+        self.RIGHT_THUMB_UPDOWN = self.DriverJoystick.getRawAxis(5)
 
-        self.LEFT_TRIGGER=self.Joystick.getRawAxis(2)
-        self.RIGHT_TRIGGER=self.Joystick.getRawAxis(3)
+        self.LEFT_TRIGGER=self.OperatorJoystick.getRawAxis(2)
+        self.RIGHT_TRIGGER=self.OperatorJoystick.getRawAxis(3)
 
-        self.A_Button=self.Joystick.getRawButton(1)
-        self.B_Button=self.Joystick.getRawButton(2)
-        self.X_Button=self.Joystick.getRawButton(3)
-        self.Y_Button=self.Joystick.getRawButton(4)
-        self.LB_Button=self.Joystick.getRawButton(5)
-        self.RB_Button=self.Joystick.getRawButton(6)
+        self.A_Button=self.OperatorJoystick.getRawButton(1)
+        self.B_Button=self.OperatorJoystick.getRawButton(2)
+        self.X_Button=self.OperatorJoystick.getRawButton(3)
+        self.Y_Button=self.OperatorJoystick.getRawButton(4)
+        self.LB_Button=self.OperatorJoystick.getRawButton(5)
+        self.RB_Button=self.OperatorJoystick.getRawButton(6)
 
+        #Driver
+        rightSpeed = self.MaxDriveSpeed*self.RIGHT_THUMB_UPDOWN
+        leftSpeed = self.MaxDriveSpeed*self.LEFT_THUMB_UPDOWN
+        self.setDrives(leftSpeed, rightSpeed)
 
-
-        # Set the motor's output to half power.
-        # This takes a number from -1 (100% speed in reverse) to +1 (100%
-        # speed going forward)
-        # if self.A_Button:
-        self.Speed=0.5
-        self.LeftFrontMotor.set(-1*self.Speed*self.LEFT_THUMB_UPDOWN)
-        self.RightFrontMotor.set(self.Speed*self.RIGHT_THUMB_UPDOWN)
-
-        # if self.RIGHT_TRIGGER > 0:
-        #     self.IntakeMotor.set(self.Speed*self.RIGHT_TRIGGER)
-        # if self.LEFT_TRIGGER > 0:
-        #     self.IntakeMotor.set(-1*self.Speed*self.LEFT_TRIGGER)
+        #Operator
+        if(self.A_Button):
+            self.wantedArmPosition = self.armHome
+            self.armRunningToPosition = True
+        elif(self.B_Button):
+            self.wantedArmPosition = self.LowerArm
+            self.armRunningToPosition = True
+        elif(self.Y_Button):
+            self.wantedArmPosition = self.UpperArm 
+            self.armRunningToPosition = True
 
         # Move the jackshaft to the up position:
-        if self.Y_Button:
-            current_position = self.encoder.getPosition() # get current position of motor
-            print("Current position:", current_position)
-            self.encoder.setPosition()
-            
-        if self.RIGHT_TRIGGER > 0:
-            self.JackShaftMotor.set(self.Speed*self.RIGHT_TRIGGER)
-        if self.LEFT_TRIGGER > 0:
-            self.JackShaftMotor.set(-1*self.Speed*self.LEFT_TRIGGER)
+        if self.X_Button:
+            self.armRunningToPosition = False
         
-
-        # if self.B_Button:
-        #     self.LeftFrontMotor.set(1.0)
-        # if self.Y_Button:
-        #     self.LeftFrontMotor.set(0)
-            
-
+        if(self.armRunningToPosition):
+            self.setArmPosition(self.wantedArmPosition)
+        else:
+            if self.RIGHT_TRIGGER > 0:
+                self.JackShaftMotor.set(self.MaxArmSpeed*self.RIGHT_TRIGGER)
+            if self.LEFT_TRIGGER > 0:
+                self.JackShaftMotor.set(-1*self.MaxArmSpeed*self.LEFT_TRIGGER)
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
